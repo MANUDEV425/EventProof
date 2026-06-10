6caef5f5fbd0f6a2a1511fc007e7e20b1c759b0c
MANUDEV425 <manudevsharma428@gmail.com>
2026-06-10 20:43:44 +0530

    Configure dynamic port for deployment

--- backend/server.js
+++ backend/server.js
@@ -31,6 +31,9 @@ app.use("/badges", badgeRoutes);
 app.get("/test", (req, res) => {
   res.send("Events API Ready");
 });
-app.listen(process.env.PORT, () => {
-  console.log(`Server running on port ${process.env.PORT}`);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
